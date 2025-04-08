96c58f80523d3f5f8e89479c59812c2366497f63
ali2a1997 <93150764+ali2a1997@users.noreply.github.com>
2025-04-08 15:58:30 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('App loaded!');
+
+window.addTask = function () {
+  const input = document.getElementById('taskInput');
+  const task = input.value.trim();
+  if (task) {
+    const li = document.createElement('li');
+    li.textContent = task;
+    document.getElementById('taskList').appendChild(li);
+    input.value = '';
+  }
+};
